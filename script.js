afbf1f7444e70ef019d11cfd6b0ec5ad6024e5bd
gigg-ity <ahmad_cheema94@hotmail.com>
2025-03-13 23:08:15 -0400

    Updated Javascript with Event Listener

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ function printMessage3(){
 function printMessage4(){
     document.getElementById('button4').innerHTML = "More information about Project 4 can be found here";
 }
+
+
+
+const form = document.getElementById("Contact Form")
+
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const numberInput = document.getElementById("number");
+
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const numberError = document.getElementById("numberError");
+
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener('submit'), function(event){
+    nameError.textContent ='';
+}
+
+if(nameInput.value.trim() ===''){
+    nameError.textContent = 'Please enter your Full Name';
+    event.preventDefault();
+}
+if (emailInput.value.trim() === '') {
+    emailError.textContent = 'Please enter your email address';
+    event.preventDefault();}
+    else if (!validateEmail(emailInput.value)) {
+        emailError.textContent = 'Please enter a valid email address';
+        event.preventDefault();
+    }
+    if (numberInput.value.trim() === '') {
+        numberError.textContent = 'Please enter your Phone Number';
+        event.preventDefault();
+    } else if (numberInput.value.length < 10) {
+        numberError.textContent = 'Please Enter a valid Phone Number';
+        event.preventDefault();
+    }
