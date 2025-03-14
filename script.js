395e1d81f25d70ccbc3380e6d5abb44ba587be04
gigg-ity <ahmad_cheema94@hotmail.com>
2025-03-13 21:43:22 -0400

    Added Button Interactivity with Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+function printMessage(){
+    document.getElementById('button').innerHTML = "More information about Project 1 can be found here";
+}
+function printMessage2(){
+    document.getElementById('button2').innerHTML = "More information about Project 2 can be found here";
+}
+function printMessage3(){
+    document.getElementById('button3').innerHTML = "More information about Project 3 can be found here";
+}
+function printMessage4(){
+    document.getElementById('button4').innerHTML = "More information about Project 4 can be found here";
+}
